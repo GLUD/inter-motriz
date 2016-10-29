14e654e99d4a53b99f96d2d5198826e7a97e8234
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-10-29 12:31:10 -0500

    Se eliminan archivos innecesarios

--- hardware/rf-transmiter/src/rf-transmiter.ino
+++ hardware/rf-transmiter/src/rf-transmiter.ino
@@ -11,33 +11,55 @@
 #include <VirtualWire.h>
 
 char *estado = "0";
+char incomingByte = '0';        // for incoming serial data
+static char response[7] = "\0"; // static vs const?
+byte index = 0;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("El dispositivo esta listo.");
   // Initialize the IO and ISR
   vw_setup(200); // Bits per sec
+  Serial.println("El dispositivo esta listo.");
 }
 
 void loop() {
+  waitAndSend();
+  // cambiarEstado();
+}
+
+void waitAndSend() {
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    // Serial.println(incomingByte);
+    response[index] = incomingByte;
+    index++;
+    if (index > 6) {
+      Serial.print("I received: ");
+      Serial.println(response);
+      send(response);
+      index = 0;
+    }
+    // // Se debe apagar dispositivo (Pin Salida Digital de Control?)
+    // delay(1000);
+  }
+}
+
+void send(char *message) {
+  vw_send((uint8_t *)message, strlen(message));
+  vw_wait_tx(); // Wait until the whole message is gone
+}
+
+void cambiarEstado() {
   // Se debe encender Dispositivo
   if (estado == "0") {
     estado = "1";
   } else {
     estado = "0";
   }
-
   // Serial.println(estado);
   char dato[7] = "";
   strcat(dato, "a=0&b=");
   strcat(dato, estado);
-
-  Serial.println(dato);
   send(dato);
-  // Se debe apagar dispositivo (Pin Salida Digital de Control?)
-  delay(1000);
-}
-
-void send(char *message) {
-  vw_send((uint8_t *)message, strlen(message));
-  vw_wait_tx(); // Wait until the whole message is gone
 }
